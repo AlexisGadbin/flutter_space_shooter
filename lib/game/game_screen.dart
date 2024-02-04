2121c16af78e79e96ed67cd3ff5833fe66ca3913
AlexisGadbin <alexis@gadbin.com>
2024-02-05 00:12:26 +0100

    Add bullet shoot

--- lib/game/game_screen.dart
+++ lib/game/game_screen.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flutter_space_shooter/common/background.dart';
+import 'package:flutter_space_shooter/game/bullet.dart';
 import 'package:flutter_space_shooter/game/enemy.dart';
 import 'package:flutter_space_shooter/game/player.dart';
 import 'package:flutter_space_shooter/game_manager.dart';
@@ -11,11 +12,13 @@ class GameScreen extends Component with HasGameRef<GameManager> {
   static const int stepLevel = 20;
   late Player _player;
   late Timer enemySpawner;
+  late Timer bulletSpawner;
   int score = 0;
 
   @override
   Future<void>? onLoad() {
     enemySpawner = Timer(2, onTick: _spawnEnemy, repeat: true);
+    bulletSpawner = Timer(1, onTick: _spawnBullet, repeat: true);
 
     add(Background(30));
     _player = Player(_onPlayerTouch);
@@ -28,6 +31,12 @@ class GameScreen extends Component with HasGameRef<GameManager> {
     for (int i = 0; i <= min(score ~/ stepLevel, 2); i++) {
       add(Enemy(_onEnemyTouch));
     }
+  }
+
+  void _spawnBullet() {
+    var bullet = Bullet();
+    bullet.position = _player.position.clone();
+    add(bullet);
   }
 
   void _onPlayerTouch() {
@@ -48,17 +57,20 @@ class GameScreen extends Component with HasGameRef<GameManager> {
   void onMount() {
     super.onMount();
     enemySpawner.start();
+    bulletSpawner.start();
   }
 
   @override
   void update(double dt) {
     super.update(dt);
     enemySpawner.update(dt);
+    bulletSpawner.update(dt);
   }
 
   @override
   void onRemove() {
     super.onRemove();
     enemySpawner.stop();
+    bulletSpawner.stop();
   }
 }
